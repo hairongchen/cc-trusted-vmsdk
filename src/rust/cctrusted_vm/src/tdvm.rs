12ef7b155a2ce4157ab0da4213d884f836461d22
hairongchen <hairong.chen@intel.com>
2024-02-29 13:23:59 +0800

    c21

--- src/rust/cctrusted_vm/src/tdvm.rs
+++ src/rust/cctrusted_vm/src/tdvm.rs
@@ -231,7 +231,7 @@ impl CVM for TdxVM {
 
         // use TDVMCALL by default except vsock config exists at ATTEST_CFG_FILE_PATH
         let mut tdvmcall_flag = true;
-        let port: u32;
+        let mut port: u32 = 0;
         const ATTEST_CFG_FILE_PATH: &str = "/etc/tdx-attest.conf";
         if Path::new(ATTEST_CFG_FILE_PATH).exists() {
             let data_lines: Vec<String> = read_to_string(ATTEST_CFG_FILE_PATH)
